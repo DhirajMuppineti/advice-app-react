521b4f720f49105433dc38d9a6ef152f18dd402a
Anish-Mayekar <mayekaranish@gmail.com>
2024-10-06 12:53:54 +0530

    changes in css property to display the button inside the box and when button is clicked loading state should be appear

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import axios from 'axios';
 
-class App extends React.Component{
+class App extends React.Component {
     state = {
-        advice:""
+        advice: "",
+        loading: false // New state for loading indication
     };
-    componentDidMount(){
+
+    componentDidMount() {
         this.fetchAdvice();
     }
 
     fetchAdvice = () => {
+        this.setState({ loading: true }); // Set loading to true when fetch starts
         axios.get('https://api.adviceslip.com/advice')
             .then((response) => {
-                const {advice} = response.data.slip;
-                this.setState({advice});
-                console.log(advice);
+                const { advice } = response.data.slip;
+                this.setState({ advice, loading: false }); // Set loading to false when fetch is done
             })
-            .catch((error) =>{
+            .catch((error) => {
                 console.log(error);
+                this.setState({ loading: false }); // Handle loading state on error
             });
     }
 
-    render(){
-        const {advice} = this.state;
+    render() {
+        const { advice, loading } = this.state; // Destructure loading state
+
         return (
             <div className='app'>
                 <div className='card'>
                     <h1 className='heading'>{advice}</h1>
-                    <button className='button' onClick={this.fetchAdvice}>
-                        <span>GIVE ME ADVICE!</span>
+                    <button
+                        className={`button ${loading ? 'loading' : ''}`}
+                        onClick={this.fetchAdvice}
+                        disabled={loading} // Disable button when loading
+                    >
+                        <span>{loading ? 'Loading...' : 'GIVE ME ADVICE!'}</span>
                     </button>
                 </div>
             </div>
